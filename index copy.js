2bc0d175242e54fbb5dbdff90fd4c211647dac1c
Mahrashee <mahrashee@gmail.com>
2023-07-17 22:05:29 +0100

    done

---
+++ index copy.js
@@ -0,0 +1,22 @@
+console.log("JavaScript file loaded!");
+$(document).ready(function() {
+    $('.resume-button').click(function(e) {
+      e.preventDefault();
+      
+      $('html, body').animate({
+        scrollTop: $('.grid-container').offset().top
+      }, 2000);
+    });
+  });
+  $(window).on('load', function() {
+    setTimeout(function() {
+      $('#preloader').fadeOut(500);
+    }, 1000); // Adjust the duration (in milliseconds) as needed
+  });
+  
+  $(document).ready(function() {
+    $(".animate-heading").fadeIn(2000); // Fade in the heading over 2 seconds
+    $(".animate-heading").slideDown(2000); // Slide down the heading over 2 seconds
+  });
+  
+
